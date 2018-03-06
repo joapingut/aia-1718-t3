2cc595ef2616b2bc72dbbfe119551602cc4a9407
joapingut <joapingut@mail.com>
2018-03-06 19:04:42 +0100

    Regresion fixed too

--- clasificadores/regresionLogistica.py
+++ clasificadores/regresionLogistica.py
@@ -5,7 +5,7 @@ from clasificadores.clasificador import Clasificador
 import clasificadores.clasificador as clasificador
 
 
-class Perceptron(Clasificador):
+class Regresion(Clasificador):
 
     def __init__(self, clases, norm=False, estocastico=True):
         self.clases = clases
@@ -21,10 +21,10 @@ class Perceptron(Clasificador):
         None
 
     def clasifica_prob(self, ej):
-        return calcular_prediccion(ej, self.pesos, None)
+        return clasificador.calcular_prediccion(ej, self.pesos, None)
 
     def clasifica(self, ej):
-        return calcular_prediccion(ej, self.pesos, self.clases)
+        return clasificador.calcular_prediccion(ej, self.pesos, self.clases)
 
     def evalua(self, validacion, resultados):
         exito = 0
@@ -53,11 +53,11 @@ def entrena(conjunto, resultados, clases, n_epochs, rate_inicial, pesos_iniciale
     while epoch < n_epochs or n_errors == 0:
         n_errors = 0
         for index in range(0, len(conjunto)):
-            prediccion = calcular_prediccion(conjunto[index], pesos, clases)
+            prediccion = clasificador.calcular_prediccion(conjunto[index], pesos, clases)
             if prediccion != resultados[index]:
                 n_errors += 1
-                if estocastico:
-                    pesos = ajusta_pesos_estocastico(conjunto[index], pesos, clasificador.busca_resultado(resultados[index], clases), rate)
+            if estocastico:
+                pesos = ajusta_pesos_estocastico(conjunto[index], pesos, clasificador.busca_resultado(resultados[index], clases), rate)
         if not estocastico:
             pesos = ajusta_pesos_batch(conjunto, pesos, resultados, clases, rate)
         if rate_decay:
@@ -65,36 +65,21 @@ def entrena(conjunto, resultados, clases, n_epochs, rate_inicial, pesos_iniciale
         epoch += 1
     return pesos
 
-def calcular_prediccion(conjunto, pesos, clases):
-    coef = clasificador.calcular_producto_escalar(pesos, conjunto)
-    result = clasificador.umbral(coef)
-    if clases != None:
-        return clases[result]
-    return result
-
-'''
-    coef = []
-    coef.append(pesos[0] + rate * (esperado - clasificador.umbral(pesos[0])))
-    for i in range(0, len(conjunto)):
-        coef.append(pesos[i + 1] + rate * conjunto[i] * (esperado - clasificador.umbral(clasificador.calcular_producto_escalar(pesos, conjunto))))
-    return coef
-'''
-
 
 def ajusta_pesos_estocastico(conjunto, pesos, esperado, rate):
     coef = []
     error = clasificador.sigma(clasificador.calcular_producto_escalar(pesos, conjunto))
-    coef.append(pesos[0] + rate * (esperado - clasificador.sigma(sum(pesos))))
+    coef.append(pesos[0] + rate * (esperado - error) * error * (1 - error))
     for i in range(0, len(conjunto)):
         coef.append(pesos[i + 1] + rate * conjunto[i] * (esperado - error) * error * (1 - error))
     return coef
 
 def ajusta_pesos_batch(conjunto, pesos, resultados, clases, rate):
     coef = copy.copy(pesos)
-    sigma_cero = clasificador.sigma(sum(pesos))
     sumatorio_cero = 0
     for j in range(0, len(conjunto)):
         objetivo = clasificador.busca_resultado(resultados[j], clases)
+        sigma_cero = clasificador.sigma(clasificador.calcular_producto_escalar(pesos, conjunto[j]))
         sumatorio_cero += (objetivo - sigma_cero) * sigma_cero * (1 - sigma_cero)
     coef[0] = coef[0] + rate * sumatorio_cero
     for i in range(1, len(pesos)):
@@ -103,6 +88,6 @@ def ajusta_pesos_batch(conjunto, pesos, resultados, clases, rate):
             objetivo = clasificador.busca_resultado(resultados[j], clases)
             sigma = clasificador.sigma(clasificador.calcular_producto_escalar(pesos, conjunto[j]))
             error = (objetivo - sigma) * sigma * (1 - sigma) 
-            error_global += error * conjunto[j][i]
+            error_global += error * conjunto[j][i - 1]
         coef[i] = coef[i] + (rate  * error_global)
     return coef
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ __author__ = 'Joaquin'
 
 import clasificadores.perceptron as per
 import clasificadores.maximizarVerosimilitud as maximizar
+import clasificadores.regresionLogistica as regresion
 import clasificadores.generadorConjuntos as Gen
 import clasificadores.clasificador as Clasificador
 import datos.votos as Votos
@@ -16,8 +17,8 @@ perceptron = per.Perceptron(Votos.votos_clases)
 
 perceptron.entrena(Votos.votos_entr, Votos.votos_entr_clas, 1000, rate_decay=False)
 
-print(perceptron.imprime())
-print(perceptron.evalua(Votos.votos_valid, Votos.votos_valid_clas))
+print("Preceptron estocastico: ",perceptron.imprime())
+print("Preceptron estocastico: ",perceptron.evalua(Votos.votos_valid, Votos.votos_valid_clas))
 
 # Pesos mejores para votos, probabilidad con validacion de 0.9710144927536232
 mejor_votos = [0.03724722117863166, 0.07855852078341741, -0.04431696109736549, 0.016418011251744247, -0.23789171693988465, -0.042607598132008206, -0.014111082392901153, 0.021817553749997387, 0.04786968628491195, 0.0650495427619262, -0.049239096885685285, -0.015182104323912426, -0.07350127226732184, -0.03564717793892669, -0.00531360899617539, 0.03314275012576218, -0.021657938490079687]
@@ -41,3 +42,15 @@ max_batch.entrena(Votos.votos_entr, Votos.votos_entr_clas, 1000, rate_decay=Fals
 
 print("Maximizar batch: ", max_batch.imprime())
 print("Maximizar batch: ", max_batch.evalua(Votos.votos_valid, Votos.votos_valid_clas))
+
+regre_esto = regresion.Regresion(Votos.votos_clases, estocastico=True)
+regre_esto.entrena(Votos.votos_entr, Votos.votos_entr_clas, 1000, rate_decay=False)
+
+print("Regresion estocastico: ", regre_esto.imprime())
+print("Regresion estocastico: ", regre_esto.evalua(Votos.votos_valid, Votos.votos_valid_clas))
+
+regre_batch = regresion.Regresion(Votos.votos_clases, estocastico=False)
+regre_batch.entrena(Votos.votos_entr, Votos.votos_entr_clas, 1000, rate_decay=False)
+
+print("Regresion batch: ", regre_batch.imprime())
+print("Regresion batch: ", regre_batch.evalua(Votos.votos_valid, Votos.votos_valid_clas))
